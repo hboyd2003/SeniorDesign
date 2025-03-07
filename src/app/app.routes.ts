7944ba8ebfd89c7248bcff03f097bcb8c1101d5b
Ashton S <ashtonbstroud@gmail.com>
2025-03-06 23:52:22 -0600

    removed old files

--- src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import {MapPageComponent} from './components/map-page/map-page.component';
-import {OtherpageComponent} from "./components/otherpage/otherpage.component";
-
-export const appRoutes: Routes = [{path: 'map', component: MapPageComponent},
-    {path: 'otherpage', component: OtherpageComponent}];
